f2519dc81c9edfdc849ff6e8bbd0749456d777e7
Jake Meyer <84546278+Jake-OMES@users.noreply.github.com>
2024-06-01 18:55:28 -0500

    Added example env file

--- src/server.js
+++ src/server.js
@@ -7,12 +7,14 @@ const WebSocket = require('ws');
 const path = require('path')
 const OpenAI = require('openai');
 
+// While in test mode, the app doesn't hit the OpenAI API at all, and instead streams Lorum Ipsum.
 const TESTMODE = true;
 
 const openai = new OpenAI();
 let assistant;
 
 async function setupAssistant() {
+    // Retrieve the assistant we already created usign the OpenAI Assistant UI (https://platform.openai.com/assistants/)
     let assistants = await openai.beta.assistants.list();
     let assistant = assistants.data.find(assistant => assistant.id == process.env.ASST_ID);
 }
@@ -20,10 +22,11 @@ if (!TESTMODE) setupAssistant();
 
 const socketserver = new WebSocket.Server({ server: server });
 
+// Setup the WebSocket server for chat functionality.
 socketserver.on('connection', async function connection(ws) {
     console.log('A new client connected.');
     ws.send('STR');
-    ws.send("Welcome to JakeGPT! I'm here to assist with any questions you make have about Jake's professional qualifications, so ask away!");
+    ws.send(`Welcome to ${process.env.FIRST_NAME}GPT! I'm here to assist with any questions you make have about ${process.env.FIRST_NAME}'s professional qualifications, so ask away!`);
 
     var thread;
     if (!TESTMODE) {
@@ -63,6 +66,7 @@ socketserver.on('connection', async function connection(ws) {
     });
 });
 
+// Setup Express webserver for the chat page.
 app.use(express.static(path.join(__dirname, 'public_html')));
 
 app.get('/', async (req, res) => {
@@ -70,19 +74,20 @@ app.get('/', async (req, res) => {
 })
 
 server.listen(process.env.PORT, process.env.SERVER_IP, function () {
-    console.log("JakeGPT now running on", server.address().address + ':' + server.address().port);
+    console.log(`${process.env.FIRST_NAME}GPT now running on`, server.address().address + ':' + server.address().port);
     if (process.pid) {
-        process.title = 'jakegpt'
+        process.title = `${process.env.FIRST_NAME.toLowerCase()}gpt`
         console.log('PID: ' + process.pid, 'TITLE: ' + process.title);
     }
 });
 
+// Make it so we exit gracefully.
 process.on('SIGTERM', () => {
     console.info('SIGTERM signal received.');
-    console.log('Closing JAKEGPT server...');
+    console.log(`Closing ${process.env.FIRST_NAME}GPT server...`);
     server.close(() => {
-        console.log('JAKEGPT server closed.');
-        console.log('Exiting JAKEGPT.');
+        console.log(`${process.env.FIRST_NAME}GPT server closed`);
+        console.log(`Exiting ${process.env.FIRST_NAME}GPT.`);
         process.exit(0);
     });
 });
